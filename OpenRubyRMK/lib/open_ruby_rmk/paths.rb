65490f998245bc7c9f6a1ef17bb0606bcf23a2f9
Quintus <sutniuq@gmx.net>
2010-12-22 16:04:13 +0100

    Removed unnecessary methods. We don't have a monolithic OpenRubyRMK anymore and we have Project objects that encapsulate path information for their respective projects.

--- OpenRubyRMK/lib/open_ruby_rmk/paths.rb
+++ OpenRubyRMK/lib/open_ruby_rmk/paths.rb
@@ -23,8 +23,9 @@ along with OpenRubyRMK.  If not, see <http://www.gnu.org/licenses/>.
 module OpenRubyRMK
   
   #This module encapsulates all the different directories we work with.
-  #Most likely you won't need the constants defined here, but rather
-  #directly rely on the module methods.
+  #Path information about the projects created are stored in their
+  #respective Project objects. This module just contains information about
+  #OpenRubyRMK's main directory structure.
   module Paths
     
     #If we're running on Windows, use rubyw
@@ -57,81 +58,6 @@ module OpenRubyRMK
     CONFIG_FILE = INSTALL_DIR + "config" + "OpenRubyRMK-rc.yml"
     #In this directory and it's subdirectories reside plugins.
     PLUGINS_DIR = INSTALL_DIR + "plugins"
-    
-    @project_path = nil #Supresses this silly "not defined" warning when calling OpenRubyRMK.has_project?.
-    
-    class << self
-      
-      #The path of the project actually edited with OpenRubyRMK.
-      #+nil+, if no project is edited (usually at startup time).
-      def project_path
-        @project_path
-      end
-      
-      #Sets the path of the project that is actually edited by OpenRubyRMK.
-      #You must call this method once, before you start working with a project.
-      def project_path=(path)
-        @project_path = Pathname.new(path)
-      end
-      
-      #The path of the temporary directory that
-      #was created at the beginning.
-      def tempdir
-        @temp_dir
-      end
-      
-      #This should only be called by the OpenRubyRMK.create_tempdir method.
-      #It assigns the temporary directory for further use.
-      def tempdir=(path)
-        raise(RuntimeError, "Can't assign the temporary directory twice!") if defined? @tempdir
-        @temp_dir = path
-      end
-      
-      #Deletes everything contained in the temporary directory
-      #recursively, but doesn't delete the directory itself.
-      def clear_tempdir
-        @temp_dir.children.each do |filename|
-          if filename.directory?
-            filename.rmtree
-          else
-            filename.delete
-          end
-        end
-      end
-      
-      #The directory where a project's maps reside.
-      def project_maps_dir
-        @project_path + "data" + "maps"
-      end
-      
-      #The file describing the parent-children hierarchy of maps.
-      def project_maps_structure_file
-        project_maps_dir + "structure.bin"
-      end
-    
-      #This is the directory where a project's mapsets reside.
-      def project_mapsets_dir
-        @project_path + "data" + "graphics" + "mapsets"
-      end
-      
-      #In this directory reside a project's character graphics.
-      def project_characters_dir
-        @project_path + "data" + "graphics" + "characters"
-      end
-      
-      #The directory the extracted mapsets of a project
-      #reside in.
-      def temp_mapsets_dir
-        @temp_dir + "mapsets"
-      end
-      
-      #The directory the extracted characters of a project
-      #reside in.
-      def temp_characters_dir
-        @temp_dir + "characters"
-      end
-      
-    end
     
   end
   
