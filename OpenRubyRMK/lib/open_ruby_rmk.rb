fc220cdc99ec5af8a6acfc635bec9ad98526fc1f
Quintus <sutniuq@gmx.net>
2010-12-22 16:06:11 +0100

    Cleaned up the main inclusion file. Same as in previous commit: No monolithism anymore.

--- OpenRubyRMK/lib/open_ruby_rmk.rb
+++ OpenRubyRMK/lib/open_ruby_rmk.rb
@@ -2,7 +2,7 @@
 #Encoding: UTF-8
 
 =begin
-This file is part of OpenRubyRMK. 
+This file is part of OpenRubyRMK.
 
 Copyright © 2010 OpenRubyRMK Team
 
@@ -20,129 +20,13 @@ You should have received a copy of the GNU General Public License
 along with OpenRubyRMK.  If not, see <http://www.gnu.org/licenses/>.
 =end
 
-#This is the namespace of the OpenRubyRMK library. 
-#Please note the word "project" always refers to games 
-#created with OpenRubyRMK. If we refer to OpenRubyRMK 
-#itself, we spell out the full name "OpenRubyRMK". 
+#This is the namespace of the OpenRubyRMK library.
+#Please note the word "project" always refers to games
+#created with OpenRubyRMK. If we refer to OpenRubyRMK
+#itself, we spell out the full name "OpenRubyRMK".
 module OpenRubyRMK
   
-  #The version of the OpenRubyRMK lib you're using. 
-  VERSION = "0.0.1-dev (6.10.10)".freeze  
-  #Since Ruby's Math module doesn't define INFINITY for whatever reason...
-  INFINITY = 1.0/0.0
-  #Negative infinity. 
-  NINFINITY = -INFINITY
-  
-  class << self
-    
-    #Initializes everything needed in order to run OpenRubyRMK. 
-    #It processes the command-line switches, sets the logger up 
-    #and loads the configuration file. Finally, it creates the 
-    #temporary directory OpenRubyRMK extracts files into. 
-    #Call this method before you start any working with OpenRubyRMK. 
-    def setup
-      parse_argv
-      create_logger
-      load_config
-      create_tempdir
-    end    
-    
-    #Returns true if either -D or -d was passed to 
-    #OpenRubyRMK, or if $DEBUG is set, which is the 
-    #case when -d was passed to Ruby itself. 
-    def debug_mode?
-      options[:debug] || $DEBUG
-    end
-    
-    #This method executes the given code block if OpenRubyRMK 
-    #is run in debug mode. When executing a debug block, it prints 
-    #information about the execution time. 
-    def debug
-      return unless $DEBUG
-      $log.debug "Executing debug code at: "
-      caller.each{|call| $log.debug(call)}
-      yield
-    end
-    
-    #Convenience method that checks wheather ::project_path returns nil. 
-    #If so, no project is considered selected and false is returned, true otherwise. 
-    def has_project?
-      !Paths.project_path.nil?
-    end    
-    
-    #The hash this method returns contains all passed command-line 
-    #switches and those not passed mapped to a default value. 
-    #It's just shorthand for <tt>OpenRubyRMK::OptionHandler.options</tt>. 
-    def options
-      OptionHandler.options
-    end
-    
-    #Your direct access to OpenRubyRMK's configuration file. This 
-    #is a hash of form 
-    #  {config_option => config_value, ...}
-    #where both objects are strings. Please don't change entries 
-    #you don't know about. If you want to add your own, do this 
-    #directly in the configuration file.
-    def config
-      @config
-    end        
-    
-    private
-    
-    #Processes the command-line options. 
-    def parse_argv
-      OptionHandler.parse(ARGV)
-    end
-    
-    #Creates OpenRubyRMK's Logger. If the -d or -D option 
-    #was passed to OpenRubyRMK, it will be set up to to 
-    #log everything (i.e. the level is set to DEBUG) to 
-    #the standard output. If -l is given without a file 
-    #argument, it logs to the standard output as well, 
-    #and if a file argument is given, that file will be 
-    #logged into. If -l is not given at all, the logger writes 
-    #to auto-rolled logfiles in the bin/logs directory which 
-    #will be created if necessary. Note that the -l option 
-    #doesn't affect the logging level as -d and -D do. 
-    #Finally, there's the -L option which sets the logging 
-    #level, starting from 0 (DEBUG) and reaching to 5 (UNKNOWN). 
-    #If -L is not passed, the level is set to WARN (3). 
-    #As with the -l option, -L is ignored if -d or -D was passed. 
-    def create_logger
-      if debug_mode?
-        $log = Logger.new($stdout)  
-        $log.level = Logger::DEBUG
-        $stdout.sync = true
-        $stderr.sync = true
-      elsif options[:logfile].nil?
-        Paths::LOG_DIR.mkpath unless Paths::LOG_DIR.directory?
-        $log = Logger.new(Paths::LOG_DIR + "OpenRubyRMK.log", 5, 1048576) #1 MiB
-        $log.level = options[:loglevel] #returns WARN if -L is not set
-      else
-        $log = Logger.new(options[:logfile])
-        $log.level = options[:loglevel] #returns WARN if -L is not set
-      end      
-      $log.datetime_format =  "%d.%m.%Y, %H:%M:%S Uhr "
-      $log.info("Started.") #OK, not 100% correct, but how to log this before the logger was created?
-    end
-    
-    #Loads OpenRubyRMK's main configuration file. 
-    def load_config
-      $log.info "Loading configuration file."
-      @config = YAML.load_file(Paths::CONFIG_FILE)
-    end
-    
-    #Creates a temporary directory where we're going to 
-    #extract graphics into. 
-    def create_tempdir
-      Paths.tempdir = Pathname.new(Dir.mktmpdir("OpenRubyRMK"))
-      $log.debug("Created temporary directory '#{Paths.tempdir}'.")
-      at_exit do
-        $log.debug("Removing temporary directory '#{Paths.tempdir}'.")
-        Paths.tempdir.rmtree
-      end
-    end
-    
-  end
+  #The version of the OpenRubyRMK lib you're using.
+  VERSION = "0.0.1-dev (22.12.10)".freeze
   
 end
