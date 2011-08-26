e30a7896b9c9eb9f8afc9270838a6da06031bbbd
Quintus <sutniuq@gmx.net>
2011-08-26 10:42:22 +0200

    Moved requires into the library file and removed the executable's .rb extension

--- karfunkel/lib/open_ruby_rmk/karfunkel/server_management/karfunkel.rb
+++ karfunkel/lib/open_ruby_rmk/karfunkel/server_management/karfunkel.rb
@@ -1,24 +1,33 @@
-#!/usr/bin/env ruby
-#Encoding: UTF-8
+# -*- coding: utf-8 -*-
+# 
+# This file is part of OpenRubyRMK.
+# 
+# Copyright © 2010,2011 OpenRubyRMK Team
+# 
+# OpenRubyRMK is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# OpenRubyRMK is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with OpenRubyRMK.  If not, see <http://www.gnu.org/licenses/>.
 
-=begin
-This file is part of OpenRubyRMK.
-
-Copyright © 2010 OpenRubyRMK Team
-
-OpenRubyRMK is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-OpenRubyRMK is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with OpenRubyRMK.  If not, see <http://www.gnu.org/licenses/>.
-=end
+require "bundler/setup"
+require "pathname"
+require "rbconfig"
+require "socket"
+require "tempfile"
+require "logger"
+require "zlib"
+require "nokogiri"
+require "chunky_png" #Chunky bacon?!
+require "eventmachine"
+require "archive/tar/minitar"
 
 #Additions to Ruby's Pathname class.
 class Pathname
